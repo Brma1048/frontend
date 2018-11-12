4f6d7cd9664456a25a120543f1838d97fc3324dd
christian.axtmann@web.de <christian.axtmann@web.de>
2018-11-12 09:38:15 +0100

    ll

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -24,7 +24,28 @@ export class InMemoryDataService implements InMemoryDbService {
                 "name": "Theo"
               }      
             ]
-        }
+        },
+        {
+          "id": "1",
+          "gpsStart": "N50.418716° , E006.750000°",
+          "gpsEnd": "N50.318516° , E006.750000°",
+          "tripBuinsness": true,
+          "startOdometer": 25698,
+          "endOdometer": 25700,
+          "wayPoints": [
+                  "N50.418716° , E006.750000°",
+                  "N50.418816° , E006.750000°",
+                  "N50.418916° , E006.750000°",
+                  "N50.419016° , E006.750000°"
+          ],
+          "customerName": "MHP",
+          "projectName": "HSKA Projekt",
+          "driver" : [
+            {
+              "name": "Jürgen"
+            }      
+          ]
+      }
       ]
         
     return {trips};
