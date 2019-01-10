96a1316ffc0e2e6762e8f502d215ed852107e894
Brma1048 <max-77a@web.de>
2019-01-10 15:54:22 +0100

    Logbook Id

--- src/app/main/create-trip/create-trip.component.ts
+++ src/app/main/create-trip/create-trip.component.ts
@@ -38,9 +38,9 @@ export class CreateTripComponent implements OnInit {
   ngOnInit() {
 
     this.form = new FormGroup({
-      logbook: new FormGroup({
-        id: new FormControl('', [Validators.required])
-      }),
+      // logbook: new FormGroup({
+      //  id: new FormControl('', [Validators.required])
+      // }),
       startWaypoint: new FormGroup({
         gpsLat: new FormControl('', [Validators.required, Validators.min(0)]),
         gpsLon: new FormControl('', [Validators.required, Validators.min(0)])
@@ -54,7 +54,6 @@ export class CreateTripComponent implements OnInit {
       endOdometer: new FormControl('', [Validators.required, Validators.min(1), Validators.max(400000)]),
       customerName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]),
       projectName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]),
-      // driverid: new FormControl()
       startDate: new FormControl('', Validators.required),
       endDate: new FormControl('', Validators.required)
     });
